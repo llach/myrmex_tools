8ae911cb89985f0e74a08bcb454c7a341f46f12e
llach <lachluca@gmail.com>
2023-03-24 12:02:20 +0100

    construct dataset without merging (left/right samples treated as individual measurements)

--- examples/cloth_augmentation.py
+++ examples/cloth_augmentation.py
@@ -1,7 +1,8 @@
 import os
 import pickle
+import numpy as np
 
-from myrmex_tools import merge_left_right
+from myrmex_tools import merge_left_right, single_frame_heatmap
 
 dataset = {}
 datadir = f"{os.environ['HOME']}/cloth/"
@@ -15,6 +16,26 @@ for label in os.listdir(datadir):
         if fname in ["pics", ".DS_Store"]: continue # skip directory containing visulizations and macOS bloat
         with open(f"{datadir}{label}/{fname}", "rb") as f: dataset[label].append(pickle.load(f)["mm"])
 
-# merge all samples of each class
+# OPTION I) merge all samples of each class
+# for label, samples in dataset.items():
+#     dataset[label] = merge_left_right(samples)
+
+# OPTION II) treat left/right as individual samples → double the number of samples
 for label, samples in dataset.items():
-    dataset[label] = merge_left_right(samples)
+    samples = np.array(samples)
+    samples[:,1,:] = np.flip(samples[:,1,:], axis=2) # we still align left and right
+    dataset[label] = np.reshape(samples, (np.prod(samples.shape[:2]),16,16))
+
+import matplotlib.pyplot as plt
+fig, axes = plt.subplots(nrows=2, ncols=5, figsize=(10,5))
+
+for i, sample in enumerate(dataset["edge"]):
+    ridx, cidx = i%2, int(i/2)
+    single_frame_heatmap(sample, fig, axes[ridx,cidx], with_colorbar=False)
+    if ridx==0: axes[ridx,cidx].set_title(f"sample {cidx+1}")
+
+axes[0,0].set_ylabel("left")
+axes[1,0].set_ylabel("right")
+
+fig.tight_layout()
+plt.show()
